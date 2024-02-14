60cee2ff615c204aa86eea0cf5aa97fd496b2217
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-02-14 14:33:38 +0530

    code first approach done and created project of DBfirst

---
+++ MVC/EFCodeFirstApproachMVC/EFCodeFirstApproachMVC/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace EFCodeFirstApproachMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EFCodeFirstApproach.Models.EmployeeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EFCodeFirstApproach.Models.EmployeeContext";
+        }
+
+        protected override void Seed(EFCodeFirstApproach.Models.EmployeeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
