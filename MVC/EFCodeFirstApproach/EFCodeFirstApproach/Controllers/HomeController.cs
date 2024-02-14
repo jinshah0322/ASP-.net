a0e446c5557a7603eb7f0a4d78ba814a4e9ce9f3
Jinay Shah <78420056+jinshah0322@users.noreply.github.com>
2024-02-14 10:31:09 +0530

    created basic mvc to display data from db

---
+++ MVC/EFCodeFirstApproach/EFCodeFirstApproach/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFCodeFirstApproach.Models;
+
+namespace EFCodeFirstApproach.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeContext db = new EmployeeContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Employees.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ MVC/EFCodeFirstApproach/EFCodeFirstApproach/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EFCodeFirstApproach.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^\+(?:[0-9] ?){6,14}[0-9]$", ErrorMessage = "Invalid Mobile Number")]
+        public string Mobile {  get; set; }
+        [Required]
+        public bool Gender { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public DateTime DOB { get; set;}
+        [Required]
+        public string InterestedTechnology { get; set; }
+    }
+}
